60f964504362a2bf975ca040e5ad941cf9d1ef16
qrst0 <66962233+qrst0@users.noreply.github.com>
2024-08-21 21:51:28 +0700

    fix: add configmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,9 +9,10 @@ import { FecommModule } from './fecomm/fecomm.module';
 import { HistoryModule } from './history/history.module';
 import { JwtscModule } from './jwtsc/jwtsc.module'
 import { ShascModule } from './shasc/shasc.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, UsersModule, FilmsModule, AuthModule, FecommModule, HistoryModule, JwtscModule, ShascModule],
+  imports: [PrismaModule, UsersModule, FilmsModule, AuthModule, FecommModule, HistoryModule, JwtscModule, ShascModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
